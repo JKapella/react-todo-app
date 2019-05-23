7bccad7a5b593f666b5d5675eddaa5f22b73c2ea
Jkapella <jvkapella@gmail.com>
2019-05-23 11:06:06 +0100

    added simple header

---
+++ react-todo-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const header = <h1> James' App, but now with React! </h1>
+
+
+ReactDOM.render(header, document.getElementById('root'))
